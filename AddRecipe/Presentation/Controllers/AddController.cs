ae090846a5c18379bc7bd0334696967d40929dbf
dmEvgenii <68201274+dmEvgenii@users.noreply.github.com>
2021-06-19 12:54:20 +0300

    Add files via upload

---
+++ AddRecipe/Presentation/Controllers/AddController.cs
@@ -0,0 +1,48 @@
+﻿using AddRecipe.Domain.Interface;
+using AddRecipe.Presentation.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddRecipe.Presentation.Controllers
+{
+    [Route("Add")]
+    [ApiController]
+    public class AddController : ControllerBase
+    {
+
+        private readonly IAddService _Add_Service;
+
+        public AddController(IAddService Journal_GetList_Service)
+        {
+            _Add_Service = Journal_GetList_Service ?? throw new ArgumentNullException(nameof(Journal_GetList_Service));
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] AddModel model)
+        {
+            if (model == null)
+                return BadRequest("Введите данные");
+
+            try
+            {
+                return Ok
+                    (
+                    _Add_Service.Add(model.ToClass())
+                    );
+            }
+
+            catch (Exception e)
+            {
+
+                return Ok(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ AddRecipe/Presentation/Models/AddModel.cs
@@ -0,0 +1,40 @@
+﻿using AddRecipe.Domain.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddRecipe.Presentation.Models
+{
+    public class AddModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Note { get; set; }
+
+
+
+        public AddModel()
+        {
+
+        }
+
+
+        public Add ToClass()
+        {
+            return new Add
+            {
+                Name = this.Name,
+                Description=this.Description,
+                Ingredients=this.Ingredients,
+                Note=this.Note
+            };
+        }
+
+
+    }
+}
