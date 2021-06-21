180efa0710f32b00647c9117139771df71f64acc
dmEvgenii <68201274+dmEvgenii@users.noreply.github.com>
2021-06-21 13:12:03 +0300

    Add files via upload

--- Gateway/Controllers/IngredientController.cs
+++ Gateway/Controllers/IngredientController.cs
@@ -12,7 +12,38 @@ namespace Gateway.Controllers
     public class IngredientController : ControllerBase
     {
 
+        [HttpPut]
 
+        public async Task<IActionResult> PutStartApp()
+        {
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Sentry("https://37366873538a48b6a45ba3163f63f482@o829940.ingest.sentry.io/5812140")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+
+            try
+            {
+                logger.Error("Новый поиск упражнений");
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var result1 = await client.GetAsync("http://127.17.0.2/Ingredient");
+                    result1.EnsureSuccessStatusCode();
+                    var result = await result1.Content.ReadAsStringAsync();
+                    return Ok(result1);
+                }
+            }
+            catch (Exception e)
+            {
+
+                logger.Fatal(e, "Произошла фатальная ошибка");
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+
+            }
+
+        }
 
 
     }
